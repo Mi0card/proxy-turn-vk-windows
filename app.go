88b32fc4822a9aaa112c352615f077aac5868926
Miocard <todayidie@gmail.com>
2026-07-09 12:17:27 +0800

    Фикс отображения версии в приложении для macOS Обновление README

--- app.go
+++ app.go
@@ -25,7 +25,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-const AppVersion = "0.1.1.1"
+const AppVersion = "0.2.0"
 
 // ── Config ────────────────────────────────────────────────────────────────────
 
