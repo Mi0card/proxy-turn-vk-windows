14648306aec4bb13f066be601231b73dca619910
Mi0card <todayidie@gmail.com>
2026-07-10 09:13:31 +0800

    v0.2.3.2 Фикс бага в upstream, см. https://github.com/amurcanov/proxy-turn-vk-android/commit/d2616f603b0005ad5ec101f1666b9d3f3f5b986e

--- app.go
+++ app.go
@@ -25,7 +25,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-const AppVersion = "0.2.3.1"
+const AppVersion = "0.2.3.2"
 
 // ── Config ────────────────────────────────────────────────────────────────────
 
