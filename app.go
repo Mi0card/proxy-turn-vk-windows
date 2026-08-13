42d5ef45381b1ee39f36928198a40b627e84b67b
Mi0card <todayidie@gmail.com>
2026-08-13 19:21:54 +0800

    Бамп версии (0.2.4.4)

--- app.go
+++ app.go
@@ -26,7 +26,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-const AppVersion = "0.2.4.3"
+const AppVersion = "0.2.4.4"
 
 // ── Config ────────────────────────────────────────────────────────────────────
 
